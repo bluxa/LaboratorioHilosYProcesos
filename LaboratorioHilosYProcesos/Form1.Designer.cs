98b2f628034cbe92760ff3e455892af0d491d14d
bluxa <81200014+bluxa@users.noreply.github.com>
2021-08-26 14:13:26 -0600

    calculo del factorial

--- LaboratorioHilosYProcesos/Form1.Designer.cs
+++ LaboratorioHilosYProcesos/Form1.Designer.cs
@@ -29,13 +29,86 @@ namespace LaboratorioHilosYProcesos
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(84, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Modo Secuencial",
+            "Uso de Hilos"});
+            this.comboBox1.Location = new System.Drawing.Point(209, 55);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(209, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ejecutar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Factorial",
+            "Fibonacci"});
+            this.comboBox2.Location = new System.Drawing.Point(209, 93);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Location = new System.Drawing.Point(84, 213);
+            this.txtResultado.Multiline = true;
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(246, 225);
+            this.txtResultado.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.TextBox txtResultado;
     }
 }
 
--- LaboratorioHilosYProcesos/Form1.cs
+++ LaboratorioHilosYProcesos/Form1.cs
@@ -16,5 +16,46 @@ namespace LaboratorioHilosYProcesos
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+            {
+                if (comboBox1.Text != "")
+                {
+                    if (comboBox2.Text != "" && comboBox2.Text == "Factorial")
+                    {
+                        FactorialRecursivo r = new FactorialRecursivo();
+                        int n = 1;
+                        while (n <= Convert.ToInt32(textBox1.Text))
+                        {
+                            txtResultado.Text = txtResultado.Text + r.factorial(Convert.ToInt32(n)) + "\r\n";
+                            n++;
+                        }
+                        
+                    }
+                    else
+                        MessageBox.Show("Seleccionar operación");
+                }
+                else
+                    MessageBox.Show("Seleccionar modo de ejecución");
+            }
+            else
+                MessageBox.Show("Ingresar datos en el txt");
+
+        }
+
+    }
+
+    public class FactorialRecursivo
+    {
+        //public static double factorial(int num)
+        public double factorial(int num)
+        {
+            if (num == 0 || num == 1)
+                return 1;
+            return num * factorial(num - 1);
+
+        }
     }
 }
